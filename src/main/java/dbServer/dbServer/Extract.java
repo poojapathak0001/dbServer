3122d72dd0e702375cf305c12f033e08f9175785
Pooja Pathak <poojapathak0001@gmail.com>
2018-02-20 10:08:19 +0530

    Updated Extract.java

--- src/main/java/dbServer/dbServer/Extract.java
+++ src/main/java/dbServer/dbServer/Extract.java
@@ -6,9 +6,7 @@ public class Extract {
 	public static void main(String[] args) {
 		
 		 String query = null;
-		 
 		 System.out.println("Enter the query:");
-		 
 		 query = "select city from ipl.csv where id = 200";
 		 //calling functions QuerySelector
 		 QuerySelector qs = new QuerySelector();
